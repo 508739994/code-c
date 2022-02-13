af937e7c5e6595e96862e4806530334a02d35f40
Allen <508739994@qq.com>
2022-02-14 03:21:01 +0800

    22_2_14

--- 初始关键字/关键字.c
+++ 初始关键字/关键字.c
@@ -22,11 +22,107 @@
 //}
 
 
-typedef unsigned int u_int;
-int main()//typedef
+//typedef unsigned int u_int;
+//int main()//typedef
+//{
+//	unsigned int num = 100;
+//	u_int num2 = 90;
+//	printf("%d\n", num);
+//	printf("%d\n", num2);
+//}
+
+//int main()//signed  --声明有符号类型
+//{
+//	signed short num1 = -32768;
+//	signed short num2 = 32767;
+//	short num3 = -32768;
+//	//signed编译器默认的可以不编写
+//	printf("num1= %d  num2= %d  num3= %d", num1,num2,num3);
+//	return 0;
+//}
+
+//int main()//unsigned  --声明无符号类型
+//{
+//	unsigned short num1 = 65535;
+//	unsigned short num2 = 32767;
+//	unsigned short num3 = -32767;
+//	//signed编译器默认的可以不编写
+//	printf("num1= %d  num2= %d  num3= %d", num1, num2, num3);
+//	return 0;
+//}
+
+//static修饰局部变量
+//static修饰全局变量
+//static修饰函数
+
+//void test()//未使用static修饰局部变量
+//{//进这个范围i创建变量
+//	int i = 1;//i是一个局部变量
+//	i++;
+//	printf("%d ", i);
+//}//出这个范围i变量的值销毁
+//int main()
+//{
+//	int a = 0;
+//	while (a < 10)
+//	{
+//		test();
+//		a++;
+//	}
+//	return 0;
+//}
+
+//void test()//使用static修饰局部变量
+//{//进这个范围i创建变量
+//	static int i = 1;//i是一个局部变量
+//	i++;
+//	printf("%d ", i);
+//}//出这个范围i变量不销毁i++的值保留供下一次使用
+//int main()
+//{
+//	int a = 0;
+//	while (a < 10)
+//	{
+//		test();
+//		a++;
+//	}
+//	return 0;
+//}
+
+//extern --声明外部符号
+extern int a;//extern引用add.c的变量
+int main()//未使用static修饰全局变量
 {
-	unsigned int num = 100;
-	u_int num2 = 90;
+	printf("%d", a);
+	return 0;
+}
+
+//extern --声明外部符号
+extern int a;//extern引用add.c的变量
+int main()//使用static修饰全局变量
+{
+	printf("%d", a);
+	return 0;
+}
+
+//声明外部符号
+extern int add(int x, int y);
+int main()//未使用static修饰函数
+{
+	int a = 9;
+	int b = 10;
+	int num = add(a,b);
 	printf("%d\n", num);
-	printf("%d\n", num2);
+	return 0;
+}
+
+//声明外部符号
+extern int add(int x, int y);
+int main()//使用static修饰函数
+{
+	int a = 9;
+	int b = 10;
+	int num = add(a, b);
+	printf("%d\n", num);
+	return 0;
 }
